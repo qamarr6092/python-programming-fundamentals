1aafd71d53fa5d906d950c8424c9cdc44b6463d4
Qamar <qamarr6092@gmail.com>
2026-06-16 05:02:40 -0700

    Delete day_10 directory

--- day_10/cities.py
+++
@@ -1,3 +0,0 @@
-def get_location ( city , country , population ) :
-    location = city + ', '  + country + ' - Population = ' + str(population)
-    return location.title().strip()
--- day_10/name.py
+++
@@ -1,7 +0,0 @@
-def get_formatted_name ( first_name , last_name , middle_name = None) :
-    if middle_name :
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
-    elif not middle_name :
-        full_name = first_name + ' ' + last_name
-    return full_name.title()
-
